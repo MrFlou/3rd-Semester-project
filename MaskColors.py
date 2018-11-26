1e3842e8b4435331438b41af0b7bb427bb111ef7
Simon Nielsen <sgfn17@student.aau.dk>
2018-11-26 10:58:54 +0100

    A few comments

--- MaskColors.py
+++ MaskColors.py
@@ -1,32 +1,32 @@
 import cv2 as cv
 import numpy as np
 
-sense = 0
 cap = cv.VideoCapture(1)
 ret = cap.set(3, 640)
 ret = cap.set(4, 480)
 
+
 def findContours(inFrame):
     grayFrame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-    grayFrame = cv.bilateralFilter(grayFrame, 10, 5, 5)
     outFrame = cv.adaptiveThreshold(grayFrame, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 11, 10)
     outFrame = cv.bitwise_not(outFrame)
     im2, cnts, something = cv.findContours(outFrame.copy(), cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
-    cnts = sorted(cnts, key = cv.contourArea, reverse = True)[:10]
-    screenCnt = np.zeros((4,2))
+    cnts = sorted(cnts, key=cv.contourArea, reverse=True)[:10]
+    screenCnt = np.zeros((4, 2))
+
     for c in cnts:
-	    # approximate the contour
-	    peri = cv.arcLength(c, True)
-	    approx = cv.approxPolyDP(c, 0.01 * peri, True)
+        # approximate the contour
+        peri = cv.arcLength(c, True)
+        approx = cv.approxPolyDP(c, 0.01 * peri, True)
 
-	    # if our approximated contour has four points, then
-	    # we can assume that we have found our screen
-	    if len(approx) == 4:
-		    screenCnt = approx
-		    break
-
+        # if our approximated contour has four points, then
+        # we can assume that we have found our screen
+        if len(approx) == 4:
+            screenCnt = approx
+            break
 
     return screenCnt
+# End of findContours
 
 
 def perspectivewarp(screenCnt):
@@ -34,26 +34,21 @@ def perspectivewarp(screenCnt):
         return frame
     cv.drawContours(frame, screenCnt, -1, (0, 255, 0), 3)
     pts = screenCnt.reshape(4, 2)
-    rect = np.zeros((4, 2), dtype = "float32")
-    mainFrame = np.float32([[0,0],[640,0],[0,480],[640,480]])
+    rect = np.zeros((4, 2), dtype="float32")
+    mainFrame = np.float32([[0, 0], [640, 0], [0, 480], [640, 480]])
 
-    # the top-left point has the smallest sum whereas the
-    #  bottom-right has the largest sum
-    s = pts.sum(axis = 1)
+    s = pts.sum(axis=1)
     rect[0] = pts[np.argmin(s)]
     rect[3] = pts[np.argmax(s)]
 
-    # compute the difference between the points -- the top-right
-    # will have the minumum difference and the bottom-left will
-    # have the maximum difference
-    diff = np.diff(pts, axis = 1)
+    diff = np.diff(pts, axis=1)
     rect[1] = pts[np.argmin(diff)]
     rect[2] = pts[np.argmax(diff)]
 
-    # multiply the rectangle by the original ratio
     M = cv.getPerspectiveTransform(rect, mainFrame)
-    warpFrame = cv.warpPerspective(frame,M,(640,480))
+    warpFrame = cv.warpPerspective(frame, M, (640, 480))
     return warpFrame
+# End of perspectivewarp
 
 
 def rgb2hsv(r, g, b):
@@ -118,12 +113,12 @@ def colorThreshold(inFrame):
 
     # define range of black color in HSV
     lower_black = np.array([0, 0, 0])
-    upper_black = np.array([180, 255, 50])
+    upper_black = np.array([180, 255, 40])
 
     maskB = cv.inRange(hsv, lower_black, upper_black)
 
     # define range of green color in HSV
-    lower_green = np.array([40, 40, 40])
+    lower_green = np.array([40, 35, 35])
     upper_green = np.array([90, 255, 255])
 
     maskG = cv.inRange(hsv, lower_green, upper_green)
@@ -167,32 +162,33 @@ while(1):
 
     # Take each frame
     ret, frame = cap.read()
-    cap.set(11, 35)  # Brightness
-    cap.set(12, 30)  # Contrast
+    cap.set(11, 40)  # Brightness
+    cap.set(12, 40)  # Contrast
 
     screenCnt = findContours(frame)
     warpFrame = perspectivewarp(screenCnt)
 
     maskR, maskG, maskB = colorThreshold(warpFrame)
-    maskR2 = maskReduction(maskR, 20)
-    maskB2 = maskReduction(maskB, 20)
-    maskG2 = maskReduction(maskG, 20)
-    maskRGB = cv.merge((maskB2, maskG2, maskR2))
 
+    # Displaying diffrent stages of the process the frame goes though, to help debug any faults
     cv.imshow('frame', frame)
-    cv.imshow('blockMask', maskRGB)
+    cv.imshow('warpFrame', warpFrame)
+    cv.imshow('Mask-Red', maskR)
+    cv.imshow('Mask-Black', maskB)
+    cv.imshow('Mask-Green', maskG)
 
     k = cv.waitKey(5) & 0xFF
+    # Esc to quit the python code
     if k == 27:
         break
-    if k == 43:
-        sense = sense + 1
-        print(sense)
-    if k == 45:
-        sense = sense - 1
-        print(sense)
+    # (Numpad P and p) to Print the processed image
     if k == 112 or k == 80:
         print('Print image')
+        maskR2 = maskReduction(maskR, 30)
+        maskB2 = maskReduction(maskB, 30)
+        maskG2 = maskReduction(maskG, 30)
+        maskRGB = cv.merge((maskB2, maskG2, maskR2))
+        cv.imshow('blockMask', maskRGB)
         cv.imwrite('tileMask.png', maskRGB)
 
 cv.destroyAllWindows()
